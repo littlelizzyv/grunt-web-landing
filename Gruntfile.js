dc00acc3c64f9e248e2412b7e0f548e2df63201a
BJ McDonald <bmcdonald@levesongroup.com>
2016-05-09 09:26:44 -0500

    landing variation

--- Gruntfile.js
+++ Gruntfile.js
@@ -35,11 +35,49 @@ module.exports = function(grunt) {
         ]
       }
     },
+    sass: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'css',
+          src: ['*.scss'],
+          dest: '../css',
+          ext: '.css'
+        }]
+      }
+    },
+    jshint: {
+      all: ['src/helpers/*.js']
+    },
 
     // Before generating any new files,
     // remove any previously-created files.
     clean: {
       all: ['<%= site.destination %>/**/*.{html,md}']
+    },
+
+    connect: {
+      server: {
+        options: {
+          port: 9001,
+          hostname: '127.0.0.1',
+          bases: ['<%= dist %>', '<%= src %>'],
+          livereload: true
+        }
+      }
+    },
+    watch: {
+      options: {
+        livereload: true
+      },
+      src: {
+        files: ['src/templates/**/*.hbs', 'data/*.json', 'css/scss/**/*.scss', 'css/*.css', 'helpers/*.js', 'Gruntfile.js'],
+        tasks: ['jshint']
+      }
+    },
+    open: {
+      path: 'http://localhost:9001',
+      app: 'Google Chrome'
     }
   });
 
@@ -47,7 +85,19 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('assemble');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-verb');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-open');
 
   // Default task to be run.
-  grunt.registerTask('default', ['clean', 'assemble']);
+  grunt.registerTask('default', ['clean', 'assemble', 'sass', 'jshint']);
+
+  grunt.registerTask('server', [
+      'default',
+      'connect',
+      'watch',
+      'open'
+    ]);
 };
